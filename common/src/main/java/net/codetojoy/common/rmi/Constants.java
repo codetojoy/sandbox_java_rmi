838f89e2f78b840cdbdae1f01a143d0faf2b6f26
Michael Easter <codetojoy@gmail.com>
2017-09-03 10:23:34 -0300

    CompoundService will now wait for UserService and BillingService

---
+++ common/src/main/java/net/codetojoy/common/rmi/Constants.java
@@ -0,0 +1,9 @@
+
+package net.codetojoy.common.rmi;
+
+public class Constants {
+    public final static String BILLING_SERVICE = "billingServiceEndpoint";
+    public final static String USER_SERVICE = "userServiceEndpoint";
+
+    public final static String COMPOUND_SERVICE = "compoundServiceEndpoint";
+}
